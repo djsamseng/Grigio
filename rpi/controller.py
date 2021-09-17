50ea45869a37c66ff51ed9e8649fb7061cbea5f7
Samuel Seng <djsamseng@gmail.com>
2021-09-17 16:19:11 -0400

    rpi reads audio data from arduino

--- rpi/controller.py
+++ rpi/controller.py
@@ -1,8 +1,13 @@
 import asyncio
 import socketio
+import serial
+import numpy as np
+
 from romi_controller import RomiMotorController
 
 SERVER_URL = "http://192.168.1.220:4000"
+PORT ="/dev/ttyACM0"
+BAUDRATE = 19200
 
 
 motorController = RomiMotorController()
@@ -14,10 +19,23 @@ async def on_motor(data):
     print("Received on_motor:", data)
     motorController.set_motors(int(data["left"]), int(data["right"]))
 
+async def read_arduino():
+    ser = serial.Serial(PORT, BAUDRATE)
+    while True:
+        try:
+            val = ser.readline().decode("UTF-8").strip("\r\n")
+            vals = val.split(",")
+            vals = np.array([int(v) for v in vals if v])
+            print(vals)
+        except Exception as e:
+            print(e)
+            continue
+
 async def main():
     await sio.connect(SERVER_URL)
     print("Socket IO SID:", sio.sid)
     await sio.emit("registerForMotorControl")
+    sio.start_background_task(read_arduino)
     await sio.wait()
 
 async def close():
